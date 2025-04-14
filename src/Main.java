1547525eb2236065c20eb5b29b03f381d4c2b718
Zeide95 <zeide.mamyraimova@alatoo.edu.kg>
2025-04-14 17:07:58 +0600

    method to copy maze has return value

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,17 @@ import java.util.*;
 public class Main {
     private static final Random random = new Random();
     private static final int rows = 11;
-    private static final int cols = 15;
-    private static final char[][] maze = generateMaze(rows, cols);
+    private static final int cols = 11;
+    private static char[][] maze;
 
     public static void main(String[] args) {
-        printMaze(maze);
-        copyMaze();
+        maze = generateMaze(rows, cols);
+        printMaze();
 
     }
 
     public static char[][] generateMaze(int rows, int cols) {
-
-        char[][] maze = new char[rows][cols];
+        maze = new char[rows][cols];
 
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
@@ -27,7 +26,7 @@ public class Main {
         maze[startX][startY] = ' '; // вход
         maze[startX][1] = ' ';
 
-        carve(startX, 1, maze);
+        carve(startX, 1);
 
         maze[endX][endY] = ' '; // выход
         maze[endX][endY - 1] = ' ';
@@ -35,7 +34,7 @@ public class Main {
         return maze;
     }
 
-    public static void carve(int x, int y, char[][] maze) {
+    public static void carve(int x, int y) {
         int[] dx = {0, 0, -2, 2};
         int[] dy = {-2, 2, 0, 0};
         List<Integer> directions = Arrays.asList(0, 1, 2, 3);
@@ -45,19 +44,28 @@ public class Main {
             int nx = x + dx[dir];
             int ny = y + dy[dir];
 
-            if (isInBounds(nx, ny, maze) && maze[nx][ny] == '#') {
+            if (isInBounds(nx, ny) && maze[nx][ny] == '#') {
                 maze[nx][ny] = ' ';
                 maze[x + dx[dir] / 2][y + dy[dir] / 2] = ' ';
-                carve(nx, ny, maze);
+                carve(nx, ny);
             }
         }
     }
 
-    public static boolean isInBounds(int x, int y, char[][] maze) {
+    public static boolean isInBounds(int x, int y) {
         return x > 0 && x < maze.length - 1 && y > 0 && y < maze[0].length - 1;
     }
 
-    public static void copyMaze() {
+    public static void printMaze() {
+        for (char[] row : maze) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static char[][] copyMaze() {
         char[][] maze2 = new char[rows][cols];
 
         for (int i = 0; i < rows; i++) {
@@ -67,22 +75,7 @@ public class Main {
         maze2[1][0] = 'S';
         maze2[rows - 2][cols - 1] = 'F';
 
-        for (char[] row : maze2) {
-            for (char c : row) {
-                System.out.print(c);
-            }
-            System.out.println();
-        }
-    }
-
-
-    public static void printMaze(char[][] maze) {
-        for (char[] row : maze) {
-            for (char c : row) {
-                System.out.print(c);
-            }
-            System.out.println();
-        }
+        return maze2;
     }
 
 }
